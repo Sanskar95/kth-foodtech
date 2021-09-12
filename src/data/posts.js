2d4c6d05244a5ca60da7871a7e13270d877e892d
Sanskar Gupta <snskr95@gmail.com>
2021-09-12 14:36:36 +0200

    fixed parsing error in data

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,7 +4,7 @@ export default [
     "title": " The quiz night went well!",
     "content": "Now we have hosted our first event! A quiz night together with Klimato and Sweden Foodtech. In total, it was 16 enthusiastic participants and we had good, informative discussions with Klimato and around the quiz questions. Klimato is a growing organization focusing on making it easier for restaurant guests to choose more sustainable alternatives. How? They are doing an LCA (life cycle analysis) to calculate the CO2-emissions for each dish, which makes it easier for customers to choose more environmentally friendly - if they want to of course. It is a good initiative for bringing attention to food and its connection to the environment, which we at KTH Foodtech think is amazing! Did you know that today’s food system is accountable for 25% of the world’s greenhouse gas emissions? The winner of the quiz night was Emil. Together with Alice and Murtaza from KTH Foodtech, he will be able to visit Klimato and have lunch with them. Congratulations! Did you miss the quiz? No worries! Here are the questions, do it and test your knowledge about food and foodtech. Unfortunately, you will not be able to compete for the visit at Klimato, but we’re sure you will learn a lot! https://kahoot.it/challenge/0863830?challenge-id=79b6453b-84fc-427a-af40-883a6166071b_1615224397055 You have until the 4th of April, then the quiz will not be accessible anymore. To not miss the next event, be sure to follow us at our social media and/or check out the “Event”-page here on the website./n/n Stay safe!/n /the KTH Foodtech team",
     "relativeImagePath": "blogImages/quiz_pic.png"
-  }
+  },
   
   {
     "title": " Quiz Night 17th february!",
