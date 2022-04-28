c9f7980b01de60ab3879f8fb6089ba097433fad4
Jabez Otieno <jabkun@gmail.com>
2022-04-28 11:54:56 +0200

    fixed an error

--- src/views/PicturesView.js
+++ src/views/PicturesView.js
@@ -146,6 +146,7 @@ const PicturesView = ({ id }) => {
         <StyledNameTitle
           data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
+          data-aos-easing="ease-out-cubic"
           data-aos-duration="2000"
           data-aos-delay="500"
         >
